257a9bf921d43d01f10fc5cfd970d500d4a66245
Eric Teixeira <ericfteixeira@icloud.com>
2024-09-15 16:46:39 -0300

    s

--- src/app/second-chance/second-chance.component.ts
+++ src/app/second-chance/second-chance.component.ts
@@ -12,18 +12,7 @@ export class SecondChanceComponent {
   whatsappLink =
     'https://api.whatsapp.com/send?phone=5551999361823&text=te%20dou%20uma%segunda%chance%20(%20e%20te%20deixo%20usar%meus%descontos%da%jbl%20%F0%9F%A4%9D';
 
-  motivos = [
-    'Nosso papo/date foi muito foda',
-    'Preciso de alguém para ofender minorias',
-    'Teu beijo é muito bom',
-    'Tenho que aprender a beber contigo kkkkk',
-    'Eu quero saber mais de como foi tua ida no show do RBD',
-    'Quero te conhecer maaaais',
-    'Como vamos fazer com o Poa em Dobro que preciso usar?',
-    'Tu ta me devendo uma ida a um sushi',
-    'Quero usar teus descontos da JBL',
-    'Combinamos uma viagem pro Rio',
-  ];
+  motivos = [];
   contador = 1;
   noDisabled = false;
   showReasons = false;
